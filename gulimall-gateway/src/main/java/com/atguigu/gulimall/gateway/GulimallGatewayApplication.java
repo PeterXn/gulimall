e0b0a7c4190a130d4483f1ffe028e323adbd7e0e
Peter <Xiao_Peter@syscom.com.cn>
2022-04-13 00:47:16 +0800

    P27 Gateway网关完成。P1-P27在使用分布式的各种技术。

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1. 开户服务注册发现
+ * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) 排除数据库的装配
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
